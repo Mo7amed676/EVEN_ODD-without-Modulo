54f54f97b03c41d46fc5001a5d36facbb21ba904
Mo7amed676 <84755046+Mo7amed676@users.noreply.github.com>
2024-01-15 06:58:58 +0200

    Update even odd without %.py

--- even odd without %.py
+++ even odd without %.py
@@ -1,14 +1,14 @@
 
 flag=True
-number=2375
+number=1357894
 str_number=str(number)
-ones=len(str_number)
+lengthOfNumberDigits=len(str_number)
 oddList=[1,3,5,7,9]
 for i in oddList:
-    if int(str_number[ones-1])==i:
+    if int(str_number[lengthOfNumberDigits-1])==i:   # [-1]
         flag=False
+        break
 if flag:
     print("even")
 else:
     print("odd")
-    
